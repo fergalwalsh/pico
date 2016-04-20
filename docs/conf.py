44d2beee0ff4972e3ead0d39524c77c3e527e826
Fergal Walsh <fergalwalsh@gmail.com>
2016-04-20 18:43:08 +0100

    remove githubpages extension.

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
