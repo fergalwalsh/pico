6451fa2d2b8a335e391bc1d2181b44113f6bbaa9
Fergal Walsh <fergalwalsh@gmail.com>
2016-05-25 16:17:57 +0100

    Add test_suite to setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
     url='http://github.com/fergalwalsh/pico',
     download_url='https://github.com/fergalwalsh/pico/tarball/%s' % version,
     packages=['pico'],
+    test_suite='nose2.collector.collector',
     install_requires=['wrapt >= 1.8.0', 'Werkzeug >= 0.11.4', 'requests >= 2.9.1'],
 )
