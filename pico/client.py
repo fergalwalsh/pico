a0df354d52f4701297f9986e9942966ac875ee1d
Fergal Walsh <fergalwalsh@gmail.com>
2016-05-25 16:24:34 +0100

    client.py: code cleanup & enable passing timeout and headers to functions.

--- pico/client.py
+++ pico/client.py
@@ -14,7 +14,6 @@ Use help(example.hello) or example.hello? as normal to check function parameters
 import pico.pragmaticjson as json
 import imp
 import requests
-import pico
 
 __author__ = 'Fergal Walsh'
 __version__ = '2.0.0-dev'
@@ -24,47 +23,80 @@ class PicoClient(object):
     def __init__(self, url):
         self.url = url
         self.session = requests.Session()
+        self.session.timeout = 60.0
 
-    def _get(self, url, args={}):
+    def _request(self, url, args={}, timeout=None, headers={}):
+        if not url.startswith('http'):
+            url = self.url + url
+        timeout = timeout or self.session.timeout
+        if timeout < 0:
+            timeout = None
+        headers.update({'content-type': 'application/json'})
         body = json.dumps(args)
-        data = self.session.post(url, data=body, timeout=5.0, headers={'content-type': 'application/json'}).json()
+        r = self.session.post(url, data=body, timeout=timeout, headers=headers)
+        r.raise_for_status()
+        data = r.json()
         if isinstance(data, dict) and 'exception' in data:
             raise Exception(data['exception'])
         else:
             return data
 
-    def _stream(self, url, args):
-        r = self.session.get(url, params=args, stream=True)
+    def _stream(self, url, args={}, timeout=None, headers={}):
+        if not url.startswith('http'):
+            url = self.url + url
+        timeout = timeout or self.session.timeout
+        if timeout < 0:
+            timeout = None
+        r = self.session.get(url, params=args, stream=True, timeout=timeout, headers=headers)
         for line in r.iter_lines(chunk_size=1):
+            line = line.decode()
             if 'data:' in line:
-                yield json.loads(line[6:])
+                s = json.loads(line[6:])
+                if s == 'PICO_CLOSE_STREAM':
+                    return
+                else:
+                    yield s
 
-    def _call_function(self, module, function, args, stream=False):
+    def _call_function(self, module, function, args):
         url = '{base_url}/{module}/{function}/'.format(base_url=self.url, module=module.replace('.', '/'), function=function)
-        if not stream:
-            return self._get(url, args)
-        else:
-            return self._stream(url, args)
+        return self._request(url, args)
 
     def load(self, module_name):
         """
         Load a remote module
-        example = pico.client.load("example")
+        example = client.load("example")
         """
-        module_dict = self._get(self.url + '/' + module_name)
+        module_dict = self._request(self.url + '/' + module_name)
+        return self.load_from_dict(module_dict)
+
+    def load_from_dict(self, module_def):
+        """
+        Load a module from a definition dictionary
+        example = client.load_from_dict({...})
+        """
+        module_name = module_def['name']
         module = imp.new_module(module_name)
-        module.__doc__ = module_dict['doc']
-        functions = module_dict['functions']
-        for function_def in functions:
-            name = function_def['name']
-            args = function_def['args']
-            args_string = ', '.join(["%s=%s" % (arg, json.dumps(default).replace("null", "None")) for arg, default in args if arg is not None])
-            stream = function_def['stream']
-            docstring = function_def['doc']
-            module._pico_client = self
-            code = 'def %s(%s):\n    """ %s """\n    return _pico_client._call_function("%s", "%s", locals(), %s)' % (
-                name, args_string, docstring, module_name, name, stream)
-            exec code in module.__dict__
+        module.__doc__ = module_def['doc']
+        module._pico_client = self
+        for function_def in module_def['functions']:
+            args = function_def['args'] + [('_timeout', None), ('_headers', {})]
+            args_string = ', '.join(["%s=%r" % (a, d) for a, d in args])
+            code = 'def {name}({arg_string}):\n'
+            code += '    """ {docstring} """\n'
+            code += '    args = locals()\n'
+            code += '    args.pop("_timeout")\n'
+            code += '    args.pop("_headers")\n'
+            if function_def.get('stream'):
+                code += '    return _pico_client._stream("{url}", args, timeout=_timeout, headers=_headers)'
+            else:
+                code += '    return _pico_client._request("{url}", args, timeout=_timeout, headers=_headers)'
+            code = code.format(
+                name=function_def['name'],
+                docstring=function_def['doc'],
+                arg_string=args_string,
+                url=function_def['url'],
+            )
+            exec(code, module.__dict__)
         return module
 
 
