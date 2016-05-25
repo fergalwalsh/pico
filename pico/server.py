b70f5474d240d6785c42f3068a037c90bbe018de
Fergal Walsh <fergalwalsh@gmail.com>
2016-05-25 16:13:08 +0100

    Automatically find an available port starting from 4242.

--- pico/server.py
+++ pico/server.py
@@ -1,8 +1,10 @@
 import sys
 import importlib
+import socket
 
 from werkzeug.serving import run_simple
 from werkzeug.wsgi import SharedDataMiddleware
+from werkzeug.utils import import_string
 
 
 def run_app(app, ip='127.0.0.1', port=4242, use_debugger=True, use_reloader=True, threaded=True):
@@ -11,12 +13,17 @@ def run_app(app, ip='127.0.0.1', port=4242, use_debugger=True, use_reloader=True
     app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
         '/': 'static'
     })
-    run_simple(ip, port, app, use_debugger=use_debugger, use_reloader=use_reloader, threaded=threaded)
+    while True:
+        try:
+            run_simple(ip, port, app, use_debugger=use_debugger, use_reloader=use_reloader, threaded=threaded)
+            break
+        except (OSError, socket.error):
+            port += 1
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        for module_name in sys.argv[1:]:
-            m = importlib.import_module(module_name.split('.py')[0])
-            app = m.app
+        module_name = sys.argv[1]
+        module_name = module_name.split('.py')[0]
+        app = import_string(module_name + ':app')
     run_app(app)
