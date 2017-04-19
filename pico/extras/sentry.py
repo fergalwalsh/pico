8e3c4a7147bd769e2156e29a06b04b3ee193a08f
Fergal Walsh <fergalwalsh@gmail.com>
2017-04-19 12:25:55 +0100

    Updated sentry mixin for raven api changes and to support ignoring exceptions.
    pico.exceptions.HTTPException and subclasses are now not passed to sentry by default.

--- pico/extras/sentry.py
+++ pico/extras/sentry.py
@@ -1,3 +1,4 @@
+from pico.exceptions import HTTPException
 
 
 def set_context(client, request):
@@ -17,6 +18,7 @@ def set_context(client, request):
 
 class SentryMixin(object):
     sentry_client = None
+    sentry_ignore_exceptions = (HTTPException,)
 
     def prehandle(self, request, kwargs):
         if self.sentry_client:
@@ -24,10 +26,14 @@ class SentryMixin(object):
         super(SentryMixin, self).prehandle(request, kwargs)
 
     def handle_exception(self, exception, request, **kwargs):
-        if self.sentry_client:
+        if self.sentry_ignore_exceptions:
+            should_ignore = isinstance(exception, tuple(self.sentry_ignore_exceptions))
+        else:
+            should_ignore = False
+        if not should_ignore and self.sentry_client:
             sentry_id = self.sentry_client.captureException()
             if sentry_id:
                 # The sentry_id is passed down to pico's JsonErrorResponse so it is
                 #  included as a value in the response.
-                kwargs['sentry_id'] = sentry_id[0]
+                kwargs['sentry_id'] = sentry_id
         return super(SentryMixin, self).handle_exception(exception, request, **kwargs)
