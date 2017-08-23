6bcd22c7f57e1d2a4f095fd35ca1c822f140e9d4
Fergal Walsh <fergalwalsh@gmail.com>
2017-08-23 13:43:51 +0100

    Bump version.

--- pico/__init__.py
+++ pico/__init__.py
@@ -34,7 +34,7 @@ except NameError:
     unicode = str
 
 __author__ = 'Fergal Walsh'
-__version__ = '2.0.1'
+__version__ = '2.0.2'
 
 
 registry = defaultdict(dict)
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-version = '2.0.1'
+version = '2.0.2'
 
 setup(
     name='pico',
     version=version,
-    description='Pico Web Application Framework',
+    description='Pico HTTP API Framework',
     author='Fergal Walsh',
     url='http://github.com/fergalwalsh/pico',
     download_url='https://github.com/fergalwalsh/pico/tarball/%s' % version,
@@ -13,4 +13,9 @@ setup(
     test_suite='nose2.collector.collector',
     install_requires=['wrapt >= 1.8.0', 'Werkzeug >= 0.12.1', 'requests >= 2.9.1'],
     include_package_data=True,
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    )
 )
